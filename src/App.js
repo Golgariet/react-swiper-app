b79c41daff82d32e7921ad66c0d7ad983654441b
Anton Bibergal <nathgilt@gmail.com>
2020-07-28 18:36:42 +0300

    extra polishing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
-//import Slider from "./dynamic_components/Slider";
 import SPA from "./static_component/SPA";
 
-// const style = {
-//   container: "container",
-//   header: "header",
-//   headerSign: "header-sign",
-//   main: "main",
-//   footer: "caption-container",
-// };
+
 
 class App extends Component {
   render() {
     return (
       <>
         <SPA />
-        {/* <Slider /> */}
       </>
     );
   }
